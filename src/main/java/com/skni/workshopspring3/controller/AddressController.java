e13568e364a18eb7f31623e8d3005075233b70f3
Robert Grochowski <robert@dbit.com.pl>
2021-11-29 21:06:35 +0100

    zad1 solution

---
+++ src/main/java/com/skni/workshopspring3/controller/AddressController.java
@@ -0,0 +1,62 @@
+package com.skni.workshopspring3.controller;
+
+import com.skni.workshopspring3.repo.AddressRepository;
+import com.skni.workshopspring3.repo.entity.Address;
+import com.skni.workshopspring3.service.AddressService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1")
+public class AddressController {
+
+    private final AddressRepository addressRepository;
+    private final AddressService addressService;
+
+    @GetMapping("/address")
+    public List<Address> getAddresses() {
+        return addressRepository.findAll();
+    }
+
+    @GetMapping("/address/{id}")
+    public ResponseEntity<Address> getAddress(@PathVariable Long id) {
+        Optional<Address> result = addressRepository.findById(id);
+
+        if(result.isPresent())
+            return new ResponseEntity<>(result.get(), HttpStatus.OK);
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/address")
+    public void addAddress(@RequestBody Address address) {
+        addressRepository.save(address);
+    }
+
+    @DeleteMapping("/address/{id}")
+    public ResponseEntity<?> addAddress(@PathVariable Long id) {
+       if(addressRepository.findById(id).isPresent()){
+           addressRepository.deleteById(id);
+           return new ResponseEntity<>(HttpStatus.OK);
+       }
+       else {
+           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+       }
+    }
+
+    @PatchMapping("/address/{id}")
+    public ResponseEntity<?> updateAddress(@PathVariable Long id, @RequestBody Address address) {
+        if(addressService.updateAddress(id, address)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
--- src/main/java/com/skni/workshopspring3/service/AddressService.java
+++ src/main/java/com/skni/workshopspring3/service/AddressService.java
@@ -2,6 +2,8 @@ package com.skni.workshopspring3.service;
 
 import com.skni.workshopspring3.repo.AddressRepository;
 import com.skni.workshopspring3.repo.entity.Address;
+
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +13,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AddressService {
-
-	// Co jeśli nie będzie final
-	// private AddressRepository addressRepository;
 
 	private final AddressRepository addressRepository;
 
@@ -38,5 +37,25 @@ public class AddressService {
 
 	public Optional<Address> getAddressByCity(String city){
 		return addressRepository.findFirstByCity(city);
+	}
+
+	public boolean updateAddress(long id, Address updatedAddress) {
+		Optional<Address> currentAddress = addressRepository.findById(id);
+		if(currentAddress.isPresent()) {
+			Address newAddress = currentAddress.get();
+			if(updatedAddress.getCity() != null) {
+				newAddress.setCity(updatedAddress.getCity());
+			}
+			if(updatedAddress.getStreet() != null) {
+				newAddress.setStreet(updatedAddress.getStreet());
+			}
+			if(updatedAddress.getCountry() != null) {
+				newAddress.setCountry(updatedAddress.getCountry());
+			}
+			newAddress.setId(id);
+			addressRepository.save(newAddress);
+			return true;
+		}
+		return false;
 	}
 }
